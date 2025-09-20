ccd1cc4ddd3dbeed4a4b3dac46d1291c53acea64
PP Solution <deepak.k@papayapalette.in>
2025-09-20 18:29:09 +0530

    update 1

--- src/components/Home/PatientTabs.js
+++ src/components/Home/PatientTabs.js
@@ -4,11 +4,11 @@ const PatientTabs = () => {
   return (
     <>
       <div className='flex gap-4 flex-wrap pt-4'>
-        <button className='bg-[var(--primary-color)] text-white px-4 py-2.5 rounded-lg lora lg:text-[20px]'>Medical Consultation</button>
-        <button className='bg-[var(--primary-color)] text-white px-4 py-2.5 rounded-lg lora lg:text-[20px]'>Video Consultation</button>
-        <button className='bg-[var(--primary-color)] text-white px-4 py-2.5 rounded-lg lora lg:text-[20px]'>SOS Response</button>
-        <button className='bg-[var(--primary-color)] text-white px-4 py-2.5 rounded-lg lora lg:text-[20px]'>Treatment Plan</button>
-        <button className='bg-[var(--primary-color)] text-white px-4 py-2.5 rounded-lg lora lg:text-[20px]'>Action</button>
+        <button className='bg-[var(--primary-color)] text-white px-4 py-2.5 rounded-lg lora lg:text-[16px]'>Medical Consultation</button>
+        <button className='bg-[var(--primary-color)] text-white px-4 py-2.5 rounded-lg lora lg:text-[16px]'>Video Consultation</button>
+        <button className='bg-[var(--primary-color)] text-white px-4 py-2.5 rounded-lg lora lg:text-[16px]'>SOS Response</button>
+        <button className='bg-[var(--primary-color)] text-white px-4 py-2.5 rounded-lg lora lg:text-[16px]'>Treatment Plan</button>
+        <button className='bg-[var(--primary-color)] text-white px-4 py-2.5 rounded-lg lora lg:text-[16px]'>Action</button>
       </div>
     </>
   )
